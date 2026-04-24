dae19c213fd75638e10b362c4bcbc23e688d960a
Ianna Osborne <ianna.osborne@cern.ch>
2026-04-24 13:00:57 +0200

    format all

--- src/kernels/slice.rs
+++ src/kernels/slice.rs
@@ -1,9 +1,7 @@
 // Copyright (c) 2026 Ianna Osborne
 // SPDX-License-Identifier: BSD-3-Clause
 
-use crate::content::{
-    Content, IndexedOptionArray, NumpyArray, RecordArray,
-};
+use crate::content::{Content, IndexedOptionArray, NumpyArray, RecordArray};
 use crate::dtype::DType;
 use std::sync::Arc;
 
