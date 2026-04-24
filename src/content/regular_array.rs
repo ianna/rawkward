46ce450b3e854a4dac3b144abc0e9010670a0c1f
Ianna Osborne <ianna.osborne@cern.ch>
2026-04-24 15:07:39 +0200

    fix

--- src/content/regular_array.rs
+++ src/content/regular_array.rs
@@ -20,7 +20,7 @@ pub struct RegularArray {
 
 impl RegularArray {
     pub fn new(content: Arc<Content>, size: usize, zeros_length: usize) -> Self {
-        let length = content.len().checked_div(size).unwrap_or(0);
+        let length = content.len().checked_div(size).unwrap_or(zeros_length);
         RegularArray {
             content,
             size,
